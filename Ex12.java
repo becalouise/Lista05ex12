1fda4129f0cb7f5c6b1cb72a386774acf8d15bcc
Rebeca Louise <114708609+becalouise@users.noreply.github.com>
2022-12-11 14:43:33 -0300

    Exercicio 12

---
+++ Ex12.java
@@ -0,0 +1,63 @@
+package Lista05ex12;
+
+import java.util.ArrayList;
+import java.util.Random;
+import java.util.Scanner;
+
+public class Ex12 {
+
+    public static String[] letras;
+
+    public static Scanner sc = new Scanner(System.in);
+
+    public static Random random = new Random();
+
+    public static ArrayList<String> arrayList = new ArrayList<String>();
+
+    public static void main(String[] args) {
+        try {
+            System.out.print("Digite a palavra: ");
+            String palavra = sc.nextLine().toUpperCase().trim();
+
+            defineLetras(palavra);
+
+            letras = arrayList.toArray(new String[arrayList.size()]);
+
+            letras = embaralhar(letras);
+
+            exibePalavra(letras);
+
+        } catch (Exception e) {
+            System.out.println("Falha:" + e.getMessage());
+        }
+    }
+
+    public static void defineLetras(String palavra) {
+        for (int i = 0; i < palavra.length(); i++) {
+
+            arrayList.add(String.valueOf(palavra.charAt(i)));
+        }
+    }
+
+    public static String[] embaralhar(String[] param) {
+        int index = 0;
+        String s = "";
+
+        for (int i = param.length - 1; i > 0; i--) {
+            index = random.nextInt(i + 1);
+            s = param[index];
+            param[index] = param[i];
+            param[i] = s;
+        }
+        return param;
+    }
+
+    public static void exibePalavra(String[] letras) {
+        String novaPalavra = "";
+        for (String letra : letras) {
+            novaPalavra = novaPalavra + letra;
+        }
+        System.out.println("\n Palavra embaralhada:" + novaPalavra);
+
+    }
+}
